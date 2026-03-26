a2f85f75c8297b003cf8711912da661e2d5245e1
Daniel Pinto de França Barros <daniellpinto_@outlook.com>
2026-03-26 13:53:33 -0300

    Atividade

--- atividade/ex03.py
+++ atividade/ex03.py
@@ -1,3 +1,3 @@
 valor_Prod = float(input("Digite o valor do produto: "))
-preco_Final = valor_Prod + (valor_Prod*8/100)
+preco_Final = valor_Prod + (valor_Prod * 8/100)
 print(f"O novo valor do produto com o acréscimo do imposto é {preco_Final}")
