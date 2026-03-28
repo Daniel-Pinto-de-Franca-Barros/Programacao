079c749125dbfcfbdc3ec19eeec6349f78d374a6
Daniel Pinto de França Barros <daniellpinto_@outlook.com>
2026-03-28 12:29:09 -0300

    Atualizar o ex01.py

--- atividade/ex01.py
+++ atividade/ex01.py
@@ -1,4 +1,4 @@
 num1 = float(input("Digite um número: "))
 num2 = float(input("Digite outro número: "))
 mult = num1 * num2
-print(f"O resultado da multiplicação é {mult}")
+print("O resultado da multiplicação é", mult)
