ba665f455181ffef30b097545196557bbd392894
Daniel Pinto de França Barros <daniellpinto_@outlook.com>
2026-03-31 16:48:28 -0300

    Atividade

--- Segunda atividade/exerc16.py
+++ Segunda atividade/exerc16.py
@@ -1,8 +1,15 @@
 #Ler um valor e mostrar o tipo, se for numérico, mostrar o quadrado
+
 valor = input("Digite um valor: ")
+print("O tipo do valor é", type(valor))
+
 try:
     num = float(valor)
-    print("É numérico")
+    print("É um valor numérico")
+    
+    quadrado = num ** 2
+    print("O quadrado é", quadrado)
+
 except:
-    print("Não é numérico")
-print("O tipo é", type(valor))
+    print("Não é um valor numérico")
+
